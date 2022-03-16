819e01113f31f68ca422e06768210eae8161f37c
Agit Rubar Demir <agit.rubar.demir@finartz.com>
2022-03-16 14:59:55 +0300

    Week 2 / Lesson 1 Codes

---
+++ week2/main.py
@@ -0,0 +1,50 @@
+a = int(5)
+
+# 5
+print(a)
+
+b = int(5.43432532532)
+
+# 5
+print(b)
+
+c = float(5.43432532532)
+
+# 5.43432532532
+print(c)
+
+# --------------
+
+d = 5 ** 2
+
+# 5^2 = 5 * 5 = 25
+print(d)
+
+# --------------
+
+if a == b:
+    print("a = b")
+else:
+    print("a != b")
+
+# --------------
+
+# pi = 3.14159
+
+# --------------
+
+helloWorldMessage = "Hello World!"
+
+print(helloWorldMessage)
+
+helloMessage = "Hello"
+
+print(helloMessage)
+
+worldMessage = "World"
+
+print(worldMessage)
+
+helloWorldMessage2 = helloMessage + " " + worldMessage + "!"
+
+print(helloWorldMessage2)
