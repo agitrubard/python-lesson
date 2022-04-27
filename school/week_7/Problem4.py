7dc34489fe3d955d6de891796b5eb9132a172574
Agit Rubar Demir <agit.rubar.demir@finartz.com>
2022-04-27 14:00:57 +0300

    Week 7 / Lesson 4 Codes

---
+++ school/week_7/Problem4.py
@@ -0,0 +1,11 @@
+# 1-100 arası çift sayıları listeleyen bir program yazınız.
+
+max_number = 100
+number = 0
+while 1 > 0:
+    number += 2
+
+    print(number)
+
+    if number >= max_number:
+        break
